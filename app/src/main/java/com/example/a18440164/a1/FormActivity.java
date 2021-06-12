fc09a90782fdcb3ad28fdf992a1d6d16d230d021
Asiri Dissanayaka <asiridissa@live.com>
2021-06-12 16:18:47 +0530

    Model bind to form

--- app/src/main/java/com/example/a18440164/a1/FormActivity.java
+++ app/src/main/java/com/example/a18440164/a1/FormActivity.java
@@ -3,28 +3,46 @@ package com.example.a18440164.a1;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
-import android.app.TimePickerDialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Calendar;
-import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class FormActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EventModel model =(EventModel)getIntent().getSerializableExtra("model");
-        setTitle("Add on " + model.Start.toString());
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+        setTitle("Add on " + df.format(model.Start));
         setContentView(R.layout.activity_form);
+
+//        EditText title  = (EditText)findViewById(R.id.editTextTitle);
+//        EditText description  = (EditText)findViewById(R.id.editTextDescription);
+//        EditText location  = (EditText)findViewById(R.id.editTextLocation);
+        EditText start  = (EditText)findViewById(R.id.editTextStart);
+        EditText end  = (EditText)findViewById(R.id.editTextEnd);
+
+//        title.setText(model.Title);
+//        description.setText(model.Description);
+//        location.setText(model.Location);
+        DateFormat tf = new SimpleDateFormat("hh:mm a", Locale.US);
+        start.setText((String)tf.format(model.Start));
+        end.setText((String)tf.format(model.End));
+
     }
 
     public void showTimePickerDialog(View v) {
-        DialogFragment newFragment = new TimePickerFragment();
-        newFragment.show(getSupportFragmentManager(), "timePicker");
+        EventModel model =(EventModel)getIntent().getSerializableExtra("model");
+        DialogFragment fragment = new TimePickerFragment();
+        Bundle bundle = new Bundle();
+        bundle.putLong("start", model.Start.getTime());
+        fragment.setArguments(bundle);
+        fragment.show(getSupportFragmentManager(), "timePicker");
     }
 }
--- app/src/main/java/com/example/a18440164/a1/TimePickerFragment.java
+++ app/src/main/java/com/example/a18440164/a1/TimePickerFragment.java
@@ -4,26 +4,33 @@ import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.widget.TimePicker;
 
 import androidx.fragment.app.DialogFragment;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Calendar;
+import java.util.Date;
 
 public class TimePickerFragment extends DialogFragment
         implements TimePickerDialog.OnTimeSetListener {
     private EventModel eventModel;
 
-    @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the current time as the default values for the picker
-        final Calendar c = Calendar.getInstance();
-        int hour = c.get(Calendar.HOUR_OF_DAY);
-        int minute = c.get(Calendar.MINUTE);
+//        final Calendar c = Calendar.getInstance();
+//        int hour = c.get(Calendar.HOUR_OF_DAY);
+//        int minute = c.get(Calendar.MINUTE);
+        long time = getArguments().getLong("start");
+        Date date= new Date(time);
+
 
         // Create a new instance of TimePickerDialog and return it
-        return new TimePickerDialog(getActivity(), this, hour, minute,
-                DateFormat.is24HourFormat(getActivity()));
+        return new TimePickerDialog(getActivity(), this, date.getHours(), date.getMinutes(), DateFormat.is24HourFormat(getActivity()));
     }
 
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
