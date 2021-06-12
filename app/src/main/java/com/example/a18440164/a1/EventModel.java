e271967afa90a15f10d5640bc24510381352be4f
Asiri Dissanayaka <asiridissa@live.com>
2021-06-12 15:04:58 +0530

    Form Design and Timepicker Fragmant

--- app/src/main/java/com/example/a18440164/a1/EventModel.java
+++ app/src/main/java/com/example/a18440164/a1/EventModel.java
@@ -1,8 +1,9 @@
 package com.example.a18440164.a1;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class EventModel {
+public class EventModel implements Serializable {
     public EventModel(){}
 
     public EventModel(Date start, Date end){
--- app/src/main/java/com/example/a18440164/a1/FormActivity.java
+++ app/src/main/java/com/example/a18440164/a1/FormActivity.java
@@ -1,9 +1,12 @@
 package com.example.a18440164.a1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -18,5 +21,10 @@ public class FormActivity extends AppCompatActivity {
         EventModel model =(EventModel)getIntent().getSerializableExtra("model");
         setTitle("Add on " + model.Start.toString());
         setContentView(R.layout.activity_form);
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
     }
 }
